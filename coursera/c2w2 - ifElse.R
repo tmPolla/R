feffb147bde736290e3455d16ff347bcb26012c4
Polla <marosi.polla@gmail.com>
2017-01-06 17:08:45 +0100

    Add files via upload

---
+++ coursera/c2w2 - ifElse.R
@@ -0,0 +1,22 @@
+##Data Science - Johns Hopkins University
+##coursera
+## course 2 - R
+##week2
+
+x<-3
+
+if(x>3) {
+  y<- 10
+  } else { ## here tis is important that the } else { will be in same line
+  y<-0
+  }
+
+print(y)
+
+
+if(x>=3 & x<5) {
+  y<- 4
+} else { ## here tis is important that the } else { will be in same line
+  y<-0
+}
+print(y)
