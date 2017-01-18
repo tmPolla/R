1f67fdc36bbbfb24a9b89e7db6529d544d00f460
Polla <marosi.polla@gmail.com>
2017-01-18 11:26:00 +0100

    completed

--- coursera/Peer-graded_Assignment/c2w3 - Assignment2 - LexicalScoping.R
+++ coursera/Peer-graded_Assignment/c2w3 - Assignment2 - LexicalScoping.R
@@ -1,41 +1,81 @@
 cat("\014")
-makeVector <- function(x = numeric()) {
-  m <- NULL
+
+makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
   set <- function(y) {
-    x <<- y
-    m <<- NULL
+    x<<-y
+    m<<-NULL
   }
-  get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
-  list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
+  get<- function() x
+  
+  setsolve <- function(solve) m<<-solve
+  getsolve <- function() m
+  list(set=set, get=get,
+       setsolve=setsolve,
+       getsolve=getsolve)
 }
 
-cachemean <- function(x, ...) {
-  m <- x$getmean()
-  if(!is.null(m)) {
+# chache solve without additional checks
+cacheSolve<- function(x,...){
+  m <- x$getsolve()
+  if(!is.null(m)){
     message("getting cached data")
     return(m)
   }
-  data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
+  data<-x$get()
+  m<-solve(data,...)
+  x$setsolve(m)
   m
 }
 
-
-
-
-
-makeCacheMatrix <- function(x = matrix()) {
-  
+# chache solve 2 with checks
+cacheSolve2<- function(x,...){
+  m <- x$getsolve()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  data<-x$get()
+  if (ncol(data)==nrow(data)) {
+    m<-solve(data,...)
+    x$setsolve(m)
+    m
+  }
+  else {
+    print ("Not a square matrix")
+  }
 }
+
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+#cat("\014")
+simpleSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
+  ## invert auto TRUE
+  print (ncol(x))
+  #check whenever it's square
+  if (ncol(x)==nrow(x)) {
+    return (solve(x))
+  }
+  else {
+    print ("Not a square matrix")
+  }
 }
+
+# try the simple solve
+x <- matrix(1:4,2,2)
+simpleSolve(x)
+
+# check the usage of the cach methods
+tm<-makeCacheMatrix(matrix(1:6,3,2))
+tm
+summary(tm)
+tm$get()
+cacheSolve(tm)
+# now I expected the message
+cacheSolve(tm)
+
+cacheSolve2(tm)
+
+
